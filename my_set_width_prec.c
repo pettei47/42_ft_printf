ffc7d6f2cfc638729cd045e636770167cf5e853e
pettei47 <kitagawa.teppei.33c@gmail.com>
2022-05-11 00:14:03 +0900

    norm all

--- my_set_width_prec.c
+++ my_set_width_prec.c
@@ -6,7 +6,7 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/15 18:15:02 by tkitagaw          #+#    #+#             */
-/*   Updated: 2022/05/10 23:31:00 by teppei           ###   ########.fr       */
+/*   Updated: 2022/05/11 00:13:14 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	my_set_width(const char **fmt, t_flag *f, va_list ap)
 {
 	if (**fmt == '*')
 	{
-		if ((f->width = va_arg(ap, int)) < 0)
+		f->width = va_arg(ap, int);
+		if (f->width < 0)
 		{
 			f->width *= -1;
 			f->minus = 1;
@@ -38,7 +39,8 @@ void	my_set_prec(const char **fmt, t_flag *f, va_list ap)
 	*fmt += 1;
 	if (**fmt == '*')
 	{
-		if ((f->prc_sz = va_arg(ap, int)) < 0)
+		f->prc_sz = va_arg(ap, int);
+		if (f->prc_sz < 0)
 			f->prec = 0;
 		*fmt += 1;
 		return ;
